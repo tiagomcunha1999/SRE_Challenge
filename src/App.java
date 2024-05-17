135dc96eab5b2c88b0b80e54c56fd3b8236d7c8d
Tiago Cunha <1700597@sal.ipg.pt>
2024-05-17 21:48:24 +0100

    Creation of the Urls file and checking if the Urls are available

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import Packages.Test;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Test t1 = new Test();
-        t1.bark();
-    }
-}
