cefa20417a881530d22494be35329306b7a4acd8
Tiago Cunha <1700597@sal.ipg.pt>
2024-05-18 00:42:08 +0100

    Creating Fetch class

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,8 @@
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Get the uls from the file
-            InputStream inputStream = Main.class.getResourceAsStream("/urls.txt");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println("URL: " + line);
-                // Check availability
-                boolean available = isURLAvailable(line);
-                System.out.println("Availability: " + (available ? "available " : "unavailable"));
-            }
-            reader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        Fetch fetch = new Fetch(); // Cria uma instância da classe Fetch
+        fetch.GetUrls(); // Chama o método GetUrls() na instância fetch
 
-    private static boolean isURLAvailable(String urlString) {
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            int responseCode = connection.getResponseCode();
-            return (responseCode == HttpURLConnection.HTTP_OK);
-        } catch (Exception e) {
-            return false;
-        }
     }
 }
 
--- src/Packages/Test.java
+++
@@ -1,7 +0,0 @@
-package Packages;
-
-public class Test {
-    public void bark(){
-        System.out.println("teste");
-    }
-}
