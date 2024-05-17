01531dfc5e5a2506a76c52da36be9181dc53109c
Tiago Cunha <1700597@sal.ipg.pt>
2024-05-17 20:44:12 +0100

    Creation of the project

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import Packages.Test;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Test t1 = new Test();
+        t1.bark();
+    }
+}
---
+++ src/Packages/Test.java
@@ -0,0 +1,7 @@
+package Packages;
+
+public class Test {
+    public void bark(){
+        System.out.println("teste");
+    }
+}
